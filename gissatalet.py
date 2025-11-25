f86e4fa7f3ebb878aa0e61a65e0d4af5e69c4d30
1Baotianx <eric.botander@elev.ga.dbgy.se>
2025-11-25 14:54:05 +0100

    gissatalet.py

--- gissatalet.py
+++ gissatalet.py
@@ -15,35 +15,42 @@ os.system('cls')
 
 playing = True
 
-while playing:
-    os.system('cls')
+while playing: #loop att man är i spelet
+    os.system('cls') 
 
-    tries = 7
-    secret_number = random.randint(1,100)
-    print(secret_number)
+    tries = 7 # hur många försök man har
+    secret_number = random.randint(1,100) # generera ett tal man ska gissa
 
 
-    while tries > 0:
+    while tries > 0: #att spelet är igång om man har mer försök än 0 så att det slutar när man har för lite försök
 
         try:
-            guess = int(input("Gissa Talet:"))
+            guess = int(input("Gissa Talet:")) #kollar så att de man skriver in är en integer
 
         except:
-            print("Skriv endast tal")
+            print("Skriv endast tal") # ifall man skriver in nåt annat så säger den till
             continue
-        if guess == secret_number:
-            print(f"Du gissade rätt med {tries} försök kvar!")
-            break
         
-        elif guess > secret_number:
+        if guess == secret_number:
+            print(f"Du gissade rätt med {tries} försök kvar!") #Om man gissar rätt säger den det och säger hur många försök man hade kvar och om man vill spela igen
+            yesno = input("Vill du spela igen j/n?: ")
+                
+            if yesno == "j": # så att spelaren får välja om den vill spela igen eller inte
+                continue 
+            else:
+                break
+                    
+            
+        
+        elif guess > secret_number: # säger om gissningen är för låg
             print("Talet är lägre")
             tries-=1
-            print(f"Du har {tries} försök kvar!")
+            print(f"Du har {tries} försök kvar!") # Hur många försök man har kvar
             continue
         else:
-            print("Talet är högre")
+            print("Talet är högre") # säger om gissningen är för hög
             tries-=1
-            print(f"Du har {tries} försök kvar!")
+            print(f"Du har {tries} försök kvar!") # Hur många försök man har kvar
             continue
 
 
