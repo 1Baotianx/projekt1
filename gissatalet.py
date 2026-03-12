215eb026c0edd2d63249cbccdf65032f820a11f1
1Baotianx <eric.botander@elev.ga.dbgy.se>
2026-03-12 14:26:54 +0100

    gissatalet.py

--- gissatalet.py
+++ gissatalet.py
@@ -25,7 +25,7 @@ while playing: #loop att man är i spelet
     while tries > 0: #att spelet är igång om man har mer försök än 0 så att det slutar när man har för lite försök
 
         try:
-            guess = int(input("Gissa Talet:")) #kollar så att de man skriver in är en integer
+            guess = int(input("(Skriv 0 för att avsluta) Gissa Talet mellan 1-100:")) #kollar så att de man skriver in är en integer
 
         except:
             print("Skriv endast tal") # ifall man skriver in nåt annat så säger den till
@@ -35,13 +35,20 @@ while playing: #loop att man är i spelet
             print(f"Du gissade rätt med {tries} försök kvar!") #Om man gissar rätt säger den det och säger hur många försök man hade kvar och om man vill spela igen
             yesno = input("Vill du spela igen j/n?: ")
                 
-            if yesno == "j": # så att spelaren får välja om den vill spela igen eller inte
+            if yesno.upper() == "J": # så att spelaren får välja om den vill spela igen eller inte
                 continue 
-            else:
+            elif yesno.upper() == "N":
+                playing = False
                 break
-                    
-            
-        
+            else:
+                os.system('cls')
+                print("Du skrev inte in J eller N. Spela igen eller Skriv 0 för att avsluta.") #motverkar error och ger instruktioner 
+                continue
+
+        elif guess == 0: # om man skriver 0 så avslutas spelet 
+            playing = False
+            break
+                     
         elif guess > secret_number: # säger om gissningen är för låg
             print("Talet är lägre")
             tries-=1
@@ -52,5 +59,5 @@ while playing: #loop att man är i spelet
             tries-=1
             print(f"Du har {tries} försök kvar!") # Hur många försök man har kvar
             continue
-
+    break       
 
